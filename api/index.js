28bf9f2f043cabc6865ad44e6ca0d531715fa7d6
Alina <alinanasirafghan@gmail.com>
2025-04-15 20:00:12 +0500

    add frontend to cors

--- api/index.js
+++ api/index.js
@@ -24,7 +24,7 @@ app.use(express.json({ limit: "50mb" }));
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(
   cors({
-    origin: `${process.env.FRONTEND_URL}`, // Replace with your frontend URL
+    origin: `https://angular-app-kappa-red.vercel.app`,
     credentials: true, // Allow sending cookies (if needed)
   })
 );
