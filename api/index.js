7a3dafa22b0cc0ca582edc938e6a92364ac9f6f1
Alina <alinanasirafghan@gmail.com>
2025-04-15 22:12:03 +0500

    fix

--- api/index.js
+++ api/index.js
@@ -5,12 +5,12 @@ const cors = require("cors"); //middleware for working w angular frontend
 const connectDB = require("../config/db.config");
 
 //ROUTERS
-const predictionRouter = require("../routes/BudgetModelRoute"); // Import the prediction route
 const userRouter = require("../routes/UserRouter");
 const trendsRoutes = require("../routes/TrendsModelRoute");
 const authRouter = require("../routes/authRouter");
 const adRouter = require("../routes/adsRouter");
 const adImageRouter = require("../routes/adImageRouter"); // Add the new ad image router
+const budgetRouter = require("../routes/budgetRouter");
 
 const app = express();
 
@@ -31,12 +31,11 @@ app.use(
 console.log("Its working");
 //ROUTES
 // /predict is the prediction router
-app.use("/budget", predictionRouter); // Use the prediction route for /predict
+app.use("/budget", budgetRouter); // Use the prediction route for /predict
 app.use("/ads", adRouter);
 app.use("/auth", authRouter);
-app.use("/predict", predictionRouter);
 app.use("/user", userRouter);
-app.use("/predict", predictionRouter);
+// app.use("/predict", predictionRouter);
 app.use("/trends", trendsRoutes);
 app.use("/adImages", adImageRouter); // Add the new route for ad images
 
